3eba4e14492af961338f25712a57f4fd56b09725
root <root@workstation.lab.example.org>
2019-05-11 17:56:18 +0200

    change on 24

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 <?php echo "Hello World!"; ?>
-<?php echo "Hello Placeholder! 5-3-2019"; ?>
+<?php echo "Hello Placeholder! 24-6-2019"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
