9fe10d48632fb033016799a124e6d498d019ee57
root <root@e4u20.e4u.local>
2019-07-19 23:01:17 +0200

    19b

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 <?php echo "Hello World!"; ?>
-<?php echo "Hello Placeholder! 19-7-2019"; ?>
+<?php echo "Hello Placeholder! 19b-7-2019"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
